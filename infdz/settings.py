654680660c7a2cbcdb68d2a9bcd1117bcfdcede1
Dmitrii Chernov <2003dima@gmail.com>
2023-03-21 16:46:38 +0300

    Add .env.example, hide all sensitive data in environment variables

--- infdz/settings.py
+++ infdz/settings.py
@@ -11,22 +11,31 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '<YOUR KEY HERE>'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
-# ALLOWED_HOSTS = ['.venusdev.space', 'localhost']
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+if not DEBUG:
+    ALLOWED_HOSTS.append(f".{env('WEB_DOMAIN')}")
+    USE_X_FORWARDED_HOST = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
 
@@ -85,8 +94,8 @@ DATABASES = {
     'production': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '<YOUR PASSWORD HERE>',
+        'USER': env("POSTGRES_USER"),
+        'PASSWORD': env("POSTGRES_PASSWORD"),
         'HOST': 'db',
         'PORT': '5432',
     },
